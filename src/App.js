fd9d534a7085b32cbbec9f19a161b0322e05ca63
crea00 <654321su@gmail.com>
2018-10-03 20:06:57 +0900

    Refactor the Appbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled, { css } from 'styled-components';
-
-const Logo = styled.div`
-  font-size: 1.5em;
-`;
-
-const ControlButton = styled.div`
-  cursor: pointer;
-  ${props => props.active && css`
-    text-shadow: 0px 0px 60px #03ff03;    
-  `}
-`;
+import AppBar from './AppBar';
 
 const AppLayout = styled.div`
   padding: 40px;
-`;
-
-const Bar = styled.div`
-  display: grid;
-  margin-bottom: 40px;
-  grid-template-columns: 100px auto 100px 100px;
 `;
 
 const Content = styled.div`
@@ -70,20 +54,7 @@ class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Bar>
-          <Logo>
-            CryptoDash 
-          </Logo>
-          <div>
-          </div>
-          {!this.state.firstVisit && (
-            <ControlButton onClick={() => {this.setState({ page: 'dashboard' })}} active={this.displayingDashboard()}>
-              Dashboard
-            </ControlButton> )}
-          <ControlButton onClick={() => {this.setState({ page: 'settings' })} } active={this.displayingSettings()}>
-            Settings
-          </ControlButton> 
-        </Bar>
+        {AppBar.call(this)}
         <Content>
           {this.displayingSettings() && this.settingsContent()}
         </Content>
