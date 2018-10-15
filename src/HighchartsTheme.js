72e41c6eb76d4ce0b4770656bc65165c06094935
crea00 <654321su@gmail.com>
2018-10-15 09:43:02 +0900

    Add custom highcharts theme

--- src/HighchartsTheme.js
+++ src/HighchartsTheme.js
@@ -1,245 +1,229 @@
-export default function() {
-  return {
-
-    colors: ['#DDDF0D', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee',
-    '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-chart: {
-    backgroundColor: {
-        linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-        stops: [
-            [0, 'rgb(48, 48, 96)'],
-            [1, 'rgb(0, 0, 0)']
-        ]
-    },
+export default {
+  lang: {
+    thousandsSep: ','
+  },
+  colors: [
+    '#61d936',
+    '#552ccb',
+    '#1163c9',
+    '#04A1EE',
+    '#08C6E0',
+    '#146B9E',
+    '#F376C1',
+    '#1B2839'
+  ],
+  chart: {
+    backgroundColor: '#061a44',
     borderColor: '#000000',
-    borderWidth: 2,
+    borderWidth: 0,
     className: 'dark-container',
-    plotBackgroundColor: 'rgba(255, 255, 255, .1)',
-    plotBorderColor: '#CCCCCC',
-    plotBorderWidth: 1
-},
-title: {
+    plotBackgroundColor: '#061a44',
+    plotBorderWidth: 0
+  },
+  title: {
     style: {
-        color: '#C0C0C0',
-        font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+      color: '#C0C0C0',
+      font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
     }
-},
-subtitle: {
+  },
+  subtitle: {
     style: {
-        color: '#666666',
-        font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
+      color: '#666666',
+      font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
     }
-},
-xAxis: {
+  },
+  xAxis: {
     gridLineColor: '#333333',
-    gridLineWidth: 1,
+    gridLineWidth: 0,
     labels: {
-        style: {
-            color: '#A0A0A0'
-        }
+      style: {
+        color: '#A0A0A0'
+      }
     },
     lineColor: '#A0A0A0',
     tickColor: '#A0A0A0',
     title: {
-        style: {
-            color: '#CCC',
-            fontWeight: 'bold',
-            fontSize: '12px',
-            fontFamily: 'Trebuchet MS, Verdana, sans-serif'
-
-        }
+      style: {
+        color: '#CCC',
+        fontWeight: 'bold',
+        fontSize: '12px',
+        fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+      }
     }
-},
-yAxis: {
+  },
+  yAxis: {
+    gridLineWidth: 0,
     gridLineColor: '#333333',
     labels: {
-        style: {
-            color: '#A0A0A0'
-        }
+      style: {
+        color: '#A0A0A0'
+      }
     },
     lineColor: '#A0A0A0',
     minorTickInterval: null,
     tickColor: '#A0A0A0',
     tickWidth: 1,
     title: {
-        style: {
-            color: '#CCC',
-            fontWeight: 'bold',
-            fontSize: '12px',
-            fontFamily: 'Trebuchet MS, Verdana, sans-serif'
-        }
+      style: {
+        color: '#CCC',
+        fontWeight: 'bold',
+        fontSize: '12px',
+        fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+      }
     }
-},
-tooltip: {
+  },
+  tooltip: {
     backgroundColor: 'rgba(0, 0, 0, 0.75)',
     style: {
-        color: '#F0F0F0'
+      color: '#F0F0F0'
     }
-},
-toolbar: {
+  },
+  toolbar: {
     itemStyle: {
-        color: 'silver'
+      color: 'silver'
     }
-},
-plotOptions: {
+  },
+  plotOptions: {
     line: {
-        dataLabels: {
-            color: '#CCC'
-        },
-        marker: {
-            lineColor: '#333'
-        }
+      dataLabels: {
+        color: '#CCC'
+      },
+      marker: {
+        lineColor: '#333'
+      }
     },
     spline: {
-        marker: {
-            lineColor: '#333'
-        }
+      marker: {
+        lineColor: '#333'
+      }
     },
     scatter: {
-        marker: {
-            lineColor: '#333'
-        }
+      marker: {
+        lineColor: '#333'
+      }
     },
     candlestick: {
-        lineColor: 'white'
+      lineColor: 'white'
     }
-},
-legend: {
+  },
+  legend: {
     itemStyle: {
-        font: '9pt Trebuchet MS, Verdana, sans-serif',
-        color: '#A0A0A0'
+      font: '9pt Trebuchet MS, Verdana, sans-serif',
+      color: '#A0A0A0'
     },
     itemHoverStyle: {
-        color: '#FFF'
+      color: '#FFF'
     },
     itemHiddenStyle: {
-        color: '#444'
+      color: '#444'
     }
-},
-credits: {
+  },
+  credits: {
     style: {
-        color: '#666'
+      color: '#666'
     }
-},
-labels: {
+  },
+  labels: {
     style: {
-        color: '#CCC'
+      color: '#CCC'
     }
-},
+  },
 
-navigation: {
+  navigation: {
     buttonOptions: {
-        symbolStroke: '#DDDDDD',
-        hoverSymbolStroke: '#FFFFFF',
-        theme: {
-            fill: {
-                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                stops: [
-                    [0.4, '#606060'],
-                    [0.6, '#333333']
-                ]
-            },
-            stroke: '#000000'
-        }
-    }
-},
-
-// scroll charts
-rangeSelector: {
-    buttonTheme: {
+      symbolStroke: '#DDDDDD',
+      hoverSymbolStroke: '#FFFFFF',
+      theme: {
         fill: {
+          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+          stops: [[0.4, '#606060'], [0.6, '#333333']]
+        },
+        stroke: '#000000'
+      }
+    }
+  },
+
+  // scroll charts
+  rangeSelector: {
+    buttonTheme: {
+      fill: {
+        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+        stops: [[0.4, '#888'], [0.6, '#555']]
+      },
+      stroke: '#000000',
+      style: {
+        color: '#CCC',
+        fontWeight: 'bold'
+      },
+      states: {
+        hover: {
+          fill: {
             linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-            stops: [
-                [0.4, '#888'],
-                [0.6, '#555']
-            ]
+            stops: [[0.4, '#BBB'], [0.6, '#888']]
+          },
+          stroke: '#000000',
+          style: {
+            color: 'white'
+          }
         },
-        stroke: '#000000',
-        style: {
-            color: '#CCC',
-            fontWeight: 'bold'
-        },
-        states: {
-            hover: {
-                fill: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0.4, '#BBB'],
-                        [0.6, '#888']
-                    ]
-                },
-                stroke: '#000000',
-                style: {
-                    color: 'white'
-                }
-            },
-            select: {
-                fill: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0.1, '#000'],
-                        [0.3, '#333']
-                    ]
-                },
-                stroke: '#000000',
-                style: {
-                    color: 'yellow'
-                }
-            }
+        select: {
+          fill: {
+            linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+            stops: [[0.1, '#000'], [0.3, '#333']]
+          },
+          stroke: '#000000',
+          style: {
+            color: 'yellow'
+          }
         }
+      }
     },
     inputStyle: {
-        backgroundColor: '#333',
-        color: 'silver'
+      backgroundColor: '#333',
+      color: 'silver'
     },
     labelStyle: {
-        color: 'silver'
+      color: 'silver'
     }
-},
+  },
 
-navigator: {
+  navigator: {
     handles: {
-        backgroundColor: '#666',
-        borderColor: '#AAA'
+      backgroundColor: '#666',
+      borderColor: '#AAA'
     },
     outlineColor: '#CCC',
     maskFill: 'rgba(16, 16, 16, 0.5)',
     series: {
-        color: '#7798BF',
-        lineColor: '#A6C7ED'
+      color: '#7798BF',
+      lineColor: '#A6C7ED'
     }
-},
+  },
 
-scrollbar: {
+  scrollbar: {
     barBackgroundColor: {
-        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-        stops: [
-                [0.4, '#888'],
-                [0.6, '#555']
-        ]
+      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+      stops: [[0.4, '#888'], [0.6, '#555']]
     },
     barBorderColor: '#CCC',
     buttonArrowColor: '#CCC',
     buttonBackgroundColor: {
-        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-        stops: [
-                [0.4, '#888'],
-                [0.6, '#555']
-        ]
+      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+      stops: [[0.4, '#888'], [0.6, '#555']]
     },
     buttonBorderColor: '#CCC',
     rifleColor: '#FFF',
     trackBackgroundColor: {
-        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-        stops: [
-            [0, '#000'],
-            [1, '#333']
-        ]
+      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+      stops: [[0, '#000'], [1, '#333']]
     },
     trackBorderColor: '#666'
-},
+  },
 
-
-
-  }
-}
+  // special colors for some of the
+  legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+  background2: 'rgb(35, 35, 70)',
+  dataLabelsColor: '#444',
+  textColor: '#C0C0C0',
+  maskColor: 'rgba(255,255,255,0.3)'
+};
